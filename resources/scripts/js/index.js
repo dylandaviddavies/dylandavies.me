5613c8daa67a8439434f185ecac0b4cc0fd14aee
dylandaviddavies <dylandaviddavies@gmail.com>
2019-07-22 18:25:13 -0400

    stuff

--- resources/scripts/js/index.js
+++ resources/scripts/js/index.js
@@ -1,3 +1,5 @@
 require('../../stylesheets/sass/index.scss');
 import DdInputContainer from "./modules/InputContainer";
+import DdNav from "./modules/Nav";
 document.querySelectorAll(".input-container").forEach(ic => new DdInputContainer(ic));
+new DdNav(document.querySelector(".nav"));
