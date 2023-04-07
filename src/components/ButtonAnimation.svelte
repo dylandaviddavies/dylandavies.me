32819bcdd40076f4f556ed886d0fa610a46857c3
Dylan Davies <49349823+dylandaviddavies@users.noreply.github.com>
2023-04-07 00:36:28 -0400

    Add button animations

---
+++ src/components/ButtonAnimation.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let direction: 'ltr' | 'ttb' | 'btt' | 'rtl' = 'rtl';
+	export let color: string = 'primary';
+</script>
+
+<div class="dd-button__animation dd-button__animation--{direction} dd-button__animation--{color}">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script>
 	import '../stylesheets/index.scss';
+	import 'material-icons/iconfont/material-icons.css';
 	import Section from '../components/Section.svelte';
 	import Button from '../components/Button.svelte';
+	import ButtonAnimation from '../components/ButtonAnimation.svelte';
 </script>
 
 <Section>
-	<h1 class="text-center">Hi, I'm Dylan Davies <br /> I'm a full stack developer.</h1>
+	<h1 class="text-center mb-6">Hi, I'm Dylan Davies <br /> I'm a full stack developer.</h1>
 
-	<Button href="#about" variation="outline">Let's make something great!</Button>
+	<Button href="#about" variation="outline">
+		<ButtonAnimation direction="ttb">
+			<span class="ml-auto material-icons-round">arrow_downward</span>
+		</ButtonAnimation>
+		<span>Let's make something great!</span>
+	</Button>
 </Section>
