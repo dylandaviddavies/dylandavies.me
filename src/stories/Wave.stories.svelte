5c28c195413059320c41655d8c35c2ee7a5be847
Dylan Davies <49349823+dylandaviddavies@users.noreply.github.com>
2023-04-09 01:12:20 -0400

    Add Wave story

---
+++ src/stories/Wave.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import Wave from '../components/common/Wave.svelte';
+</script>
+
+<Meta title="Components/Wave" component={Wave} />
+
+<Story name="Basic">
+	<div style="height: 300px">
+		<Wave color="primary" />
+	</div>
+</Story>
