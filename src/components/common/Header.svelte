2498ba58742845188103e3c526618ea413d2e4fb
Dylan Davies <49349823+dylandaviddavies@users.noreply.github.com>
2023-04-20 21:52:05 -0400

    Add header component

---
+++ src/components/common/Header.svelte
@@ -0,0 +1,3 @@
+<header class="dd-header">
+	<slot />
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,19 @@
 	import ProjectsSection from '../components/core/ProjectsSection.svelte';
 	import Footer from '../components/core/Footer.svelte';
 	import ContactSection from '../components/core/ContactSection.svelte';
+	import Header from '../components/common/Header.svelte';
 </script>
 
 <NavShelf />
 
-<Nav>
-	<NavItem href="#home">Home</NavItem>
-	<NavItem href="#about">About</NavItem>
-	<NavItem href="#projects">Projects</NavItem>
-	<NavItem href="#contact">Contact</NavItem>
-</Nav>
+<Header>
+	<Nav>
+		<NavItem href="#home">Home</NavItem>
+		<NavItem href="#about">About</NavItem>
+		<NavItem href="#projects">Projects</NavItem>
+		<NavItem href="#contact">Contact</NavItem>
+	</Nav>
+</Header>
 
 <HomeSection />
 
